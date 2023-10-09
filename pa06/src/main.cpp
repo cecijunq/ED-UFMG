8b49cba090d51d3b24cabc204d42d47cb0d08ea2
Cecilia Junqueira Vieira Machado Pereira <cecilia@MacBook-Air-de-Cecilia.local>
2023-10-09 11:55:10 -0300

    commitei pa6

--- pa06/src/main.cpp
+++ pa06/src/main.cpp
@@ -11,26 +11,12 @@
 
 // variaveis globais para opcoes
 static int opescolhida;
-char outnome[100];
 
-void parse_args(int argc,char ** argv)
-// Descricao: le as opcoes da linha de comando e inicializa variaveis
-// Entrada: argc e argv
-// Saida: optescolhida, optx, opty
-{
-     // variaveis externas do getopt
-     extern char * optarg;
-     extern int optind;
+void parse_args(int argc,char ** argv) {
+    // variavel auxiliar
+    int c;
 
-     // variavel auxiliar
-     int c;
-
-     // inicializacao variaveis globais para opcoes
-     opescolhida = -1;
-
-     // getopt - letra indica a opcao, : junto a letra indica parametro
-     // no caso de escolher mais de uma operacao, vale a ultima
-     while ((c = getopt(argc, argv, "dnk")) != EOF){
+    while ((c = getopt(argc, argv, "dnk")) != EOF){
        switch(c) {
          case 'n':
 	        opescolhida = OPVIZINHOS;
@@ -74,37 +60,5 @@ int main(int argc, char **argv) {
             }
             break;
     }
-    
-    /*
-    if(argc != 2) {
-        std::cout << "Quantidade inválida de argumentos" << std::endl;
-        return 1;
-    }
-
-    Grafo g = Grafo();
-    g.InsereVertice();
-
-    if(strcmp(argv[1], "-d") == 0) {
-        std::cout << g.QuantidadeVertices() << std::endl;
-        std::cout << g.QuantidadeArestas() << std::endl;
-        std::cout << g.GrauMinimo() << std::endl;
-        std::cout << g.GrauMaximo() << std::endl;
-
-    } else if(strcmp(argv[1], "-n") == 0) {
-        for(int i = 0; i < g.QuantidadeVertices(); i++) {
-            g.ImprimeVizinhos(i);
-            std::cout << std::endl;
-        }
-
-    } else if(strcmp(argv[1], "-k") == 0) {
-        if(g.GrauMaximo() == g.GrauMinimo() && g.GrauMaximo() == g.QuantidadeVertices() - 1) {
-            std::cout << "1" << std::endl;
-        } else {
-            std::cout << "0" << std::endl;
-        }
-    } else {
-        std::cout << "Argumento inválido" << std::endl;
-    }
-    */
     
 }
